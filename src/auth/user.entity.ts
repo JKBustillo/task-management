97c922db3b14bfeff861e8acde9dfcc2a8f4206e
Jose Bustillo <josebustillodealba@gmail.com>
2021-11-23 22:17:13 -0500

    feat: handle duplicated users

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -5,7 +5,7 @@ export class User {
   @PrimaryGeneratedColumn('increment')
   id: string;
 
-  @Column()
+  @Column({ unique: true })
   username: string;
 
   @Column()
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
@@ -10,6 +11,14 @@ export class UsersRepository extends Repository<User> {
 
     const user = this.create({ username, password });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException("Username already exists");
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
